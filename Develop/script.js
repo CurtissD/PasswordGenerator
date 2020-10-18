4cf3c88d3fd13a2796c63a2573d0a636063ef905
Curtiss D <curtissldenson@hotmail.com>
2020-10-17 21:36:15 -0500

    fixed a simple typo that was preventing my alert promts from loading

--- Develop/script.js
+++ Develop/script.js
@@ -4,43 +4,46 @@ var numericChar = "0123456789";
 var specialChar = " !@#$%^&*()_-+=;'\:<,./>?[]`~{}|";
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+};
 
-}
+  var passwordLength = prompt ("How long would you like your password? Must be between 8-128 caracters");
 
-// adding funtion to genereate a password
-funtion generatePassword(); {
-// promt requesting how long of a password
-  var passwordLength = prompt("How long would you like your password? Must be between 8-128 caracters");
-  while (passwordLength < 8 || passwordLength > 128) {
+  while (passwordLength < 8 || passwordLength > 128) 
+  {
 // alert if incorrect amount of characters is selected
-    alert("please select between 8-128 characters")
+    alert("please select between 8-128 characters");
     passwordLength = prompt("How long would you like your password? Must be between 8-128 caracters");
-    
-  
+  };
 // var for choosing which characters for password
   var lowercaseConfirm = confirm("Would you like lowercase characters? ");
   var specialCharConfirm = confirm("Would you like special characters? ");
   var numericCharConfirm = confirm("Would you like numerical characters? ");
 
+
+// promt requesting how long of a password
+ 
+
+// adding funtion to genereate a password
+ function generatePassword() {
+
+
 // If statement for characters
-  if (passwordLength >=8 && passwordLength <=128 && lowercaseConfirm && specialCharConfirm && numericCharConfirm);
-    function generate(length = passwordLength){
+  if (passwordLength >=8 && passwordLength <=128 && lowercaseConfirm && specialCharConfirm && numericCharConfirm) {
+    function generate(length = passwordLength) {
       var all = numericChar + specialChar + lowercaseChar;
-        for (var i = 0; i , length; i++){
-          var character = Math.floor(Math.random() * all,length);
-          
-        }
-    }
-  }
-else {
-  alert("Enter password length 8-128 characters long");
-  }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-}
+        for (var i = 0; i , length; i++) {
+          var character = Math.floor(Math.random() * all, length);
+        };
+    };
+  };
+ };
